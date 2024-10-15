755438a02e307bafff6aa538be63300f192dd711
Sébastien <sebastien.works@outlook.com>
2024-10-15 17:23:53 +0200

    Update core.py

--- canvasbuilder/core.py
+++ canvasbuilder/core.py
@@ -1,2 +1,17 @@
-from tkinter import Canvas as TkCanvas
 from typing import Union
+
+# Custom exception for error in the canvas initialization
+class InitError(Exception):
+  """Raised error for initialization errors."""
+  pass
+
+# Variable to know if the canvas is initialized
+is_canvas_initialized = False
+default_background_color = (255, 255, 255)
+
+# Main class needed to create the canvas
+class Canvas():
+  def __init__(self, width: Union[int, str], height: Union[int, str], background_color: Union[str, tuple]):
+
+async def PassFunc():
+  pass # pass
