4f54cb396de749cd2ce275f948d1b96a2d6cc8cd
Sébastien <sebastien.works@outlook.com>
2024-10-16 13:46:21 +0200

    Update core.py

--- canvasbuilder/core.py
+++ canvasbuilder/core.py
@@ -1,11 +1,10 @@
 from tkinter import Tk
-from typing import Union
 
 class Window:
   def __init__(self, width: int = 800, height: int = 600, title: str = "My Canvas"):
-    root = Tk()
-    root.geometry(f"{width}x{height}")
-    root.title(title)
+    self.root = Tk()
+    self.root.geometry(f"{width}x{height}")
+    self.root.title(title)
 
   def run(self):
-    root.mainloop()
+    self.root.mainloop()
