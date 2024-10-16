fbca273af1693a79b5bb82cebc82b2013cc87be2
Sébastien <sebastien.works@outlook.com>
2024-10-16 13:59:58 +0200

    Update core.py

--- canvasbuilder/core.py
+++ canvasbuilder/core.py
@@ -1,10 +1,23 @@
 from tkinter import Tk
 
+window_initialized = False
+canvas_initialized = True
+
 class Window:
   def __init__(self, width: int = 800, height: int = 600, title: str = "My Canvas"):
+    global window_initialized
+    
     self.root = Tk()
     self.root.geometry(f"{width}x{height}")
     self.root.title(title)
 
+    window_initialized = True
+
   def run(self):
     self.root.mainloop()
+
+class Canvas:
+  def __init__(self, width, height, background_color, position):
+    if not window_initialized:
+      raise WindowError("a window not init")
+      
