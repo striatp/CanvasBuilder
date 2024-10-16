810f7c25a9f87ab16ea31bd6345cb0d166c8c836
Sébastien <sebastien.works@outlook.com>
2024-10-16 18:41:23 +0200

    Update __init__.py

--- canvasbuilder/__init__.py
+++ canvasbuilder/__init__.py
@@ -1,1 +1,13 @@
+# canvasbuilder/__init__.py
 
+# Importing the main classes and functions
+from .core import Window, Canvas, rgb_to_hex
+from .Exceptions.WindowErrorHandling import WindowError
+
+# Make these available when importing the package
+__all__ = [
+    "Window",
+    "Canvas",
+    "rgb_to_hex",
+    "WindowError"
+]
