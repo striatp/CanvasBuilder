4a902ec7413b3afe602d4453a7243e79d2a6ddc9
Sébastien <sebastien.works@outlook.com>
2024-10-16 09:01:39 +0200

    Update core.py

--- canvasbuilder/core.py
+++ canvasbuilder/core.py
@@ -1,5 +1,8 @@
 from typing import Union, Tuple
 
 class Canvas:
-  def __init__(self, width: Union[int, str] = 400, height: Union[int, str] = 300, background_color: Union[str, Tuple[int, int, int]]) -> None:
-    pass
+  def __init__(self, width: Union[int, str] = 400, height: Union[int, str] = 300, background_color: Union[str, Tuple[int, int, int]] = "white") -> None:
+    if not isinstance(width, (int, str)) or width <= 0:
+      raise ValueError("The 'width' argument must be a positive integer or a string.")
+    if not isinstance(height, (int, str)) or height <= 0:
+      raise ValueError("The 'height' argument must be a positive integer or a string.")
