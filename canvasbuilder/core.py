0ff497a3c423d3acc195b7fd5d85ab24513ea6bc
Sébastien <sebastien.works@outlook.com>
2024-10-16 08:52:09 +0200

    Update core.py

--- canvasbuilder/core.py
+++ canvasbuilder/core.py
@@ -1,1 +1,5 @@
+from typing import Union, Tuple
 
+class Canvas:
+  def __init__(self, width: Union[int, str] = 400, height: Union[int, str] = 300, background_color: Union[str, Tuple[int, int, int]]) -> None:
+    pass
