ca63fde8a5bc01bf0a736ab04f538998da2a728b
Sébastien <sebastien.works@outlook.com>
2024-10-18 18:50:41 +0200

    Update rectangle.py

--- canvasbuilder/Shapes/rectangle.py
+++ canvasbuilder/Shapes/rectangle.py
@@ -4,9 +4,25 @@ from ..Exceptions.CanvasErrorHandling import CanvasError
 
 class Rectangle:
     def __init__(self, x: int = None, y: int = None, width: int = None, height: int = None, fill_color: Union[str, Tuple[int, int, int]]):
-        
-        # Initialization code here
+       if not Canvas.canvas_initialized:
+           raise CanvasError("The canvas must be initialized before adding elements on it.")
 
+      if not isinstance(x, int) or x == None:
+          raise ValueError("The 'x' argument must be an integer.")
+      if not isinstance(y, int) or y == None:
+          raise ValueError("The 'y' argument must be an integer.")
+      if not isinstance(width, int) or width == None or width <= 0:
+          raise ValueError("The 'width' argument must be a positive integer.")
+      if not isinstance(height, int) or height == None or height <= 0:
+          raise ValueError("The 'height' argument must be a positive integer.")
+      if not isinstance(fill_color, (str, tuple)):
+          raise ValueError("The 'fill_color' argument must be a string or a tuple of 3 integers.")
+
+      if isinstance(fill_color, str):
+          if not fill_color in color_dictionary:
+              raise 
+
+    
     def draw(self):
         # Code to draw the rectangle
 
